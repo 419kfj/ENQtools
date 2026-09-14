a33059ed7c82ed4ddcf09338bd95ba8721b46579
kazuo fujimoto <kazuo.fujimoto2007@gmail.com>
2026-09-14 09:45:28 +0900

    /R にbelow5%.Rを追加

---
+++ R/below5p.R
@@ -0,0 +1,23 @@
+#
+#
+#
+#
+below5p <- function(df){
+  df %>%
+    mutate(across(everything(), ~ replace_na(as.character(.), "NA"))) -> active
+  names(active) -> col_labels # 変数ラベル名をcol_labelsに格納
+
+  below5p_vec　<-　NULL
+  for (i in col_labels){
+    active %>% count(!!as.name(i)) %>% mutate(rate=round(100*n/sum(n),2)) -> tmp
+    print(tmp)
+    tmp %>% rename(V1 = 1) -> tmp2
+    tmp %>% filter(rate <= 5) %>% select(1) %>% unlist(1) -> below5p
+    below5p_vec_add <- str_c(i,".",below5p)
+    below5p_vec <-　append(below5p_vec,below5p_vec_add)
+    ggplot(tmp2,aes(x=V1,y=rate)) + geom_col(fill="lightblue") + ggtitle(i) -> p
+    plot(p)
+  }
+
+  return(below5p_vec)
+}
